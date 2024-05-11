cae0ec5f4f309b5d8a6ee96de6d1c5692367c306
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-05-11 11:23:18 +0530

    property-plus app in next js

---
+++ property-pulse/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// export const metadata = {
+//   title:'Homepage'
+// }
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
