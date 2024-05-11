804d2ef82af4c4a92a4de9b1e9009c755c69ee9a
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-05-11 17:08:25 +0530

    property page got completed by json data

--- property-pulse/app/properties/page.jsx
+++ property-pulse/app/properties/page.jsx
@@ -1,11 +1,24 @@
-import Link from 'next/link'
 import React from 'react'
+import properties from "@/properties.json"
+import PropertyCard from '@/components/PropertyCard'
 
 const PropertiesPage = () => {
   return (
     <>
-      <h1 className='text-3xl'>Properties</h1>
-      <Link href={'/'}> Go Home</Link>
+      <section class="px-4 py-6">
+        <div class="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (    
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard property={property}/>
+            ))}
+          </div>
+          )}
+        </div>
+      </section>
+
     </>
   )
 }
